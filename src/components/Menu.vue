330dc1c02c93e6f699d448fbbf3c098b35c668d2
DasomJung24 <jeongdasom6@gmail.com>
2021-04-10 05:54:24 +0900

    element ui 설치 및 layout 구현 중

---
+++ src/components/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-col :span="12">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+    >
+      <el-submenu index="1">
+        <template v-slot:title>
+          <i class="el-icon-location"></i>
+          <span>Navigator One</span>
+        </template>
+        <el-menu-item-group title="Group One">
+          <el-menu-item index="1-1">item one</el-menu-item>
+          <el-menu-item index="1-2">item one</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="Group Two">
+          <el-menu-item index="1-3">item three</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template v-slot:title>item four</template>
+          <el-menu-item index="1-4-1">item one</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+    </el-menu>
+  </el-col>
+</template>
---
+++ src/components/Nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-container>
+    <el-header style="text-align: right;">
+      <span>Login</span>
+      <span>Register</span>
+    </el-header>
+  </el-container>
+  <el-container>
+    <el-aside width="500px">
+      <Menu style="width: 500px"></Menu>
+    </el-aside>
+  </el-container>
+</template>
+<script>
+import Menu from "@/components/Menu";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Menu,
+  },
+};
+</script>
+<style scoped>
+.el-header {
+  background-color: #545c64;
+  color: #fff;
+  line-height: 60px;
+}
+/* <el-header
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        > */
+</style>
