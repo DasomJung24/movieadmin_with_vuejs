f77e267c26318a0e8d97d23ca18a3271e3bf7812
DasomJung24 <jeongdasom6@gmail.com>
2021-04-17 22:22:12 +0900

    axios 설치

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import _axios from "./common/api.service";
+import VueAxios from "vue-axios";
 
 import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
@@ -10,12 +12,13 @@ createApp(App)
   .use(ElementPlus)
   .use(store)
   .use(router)
+  .use(VueAxios, _axios)
   .mount("#app");
 
-router.beforeEach((to, from, next) => {
-  if (to.name !== "Login") {
-    next({ name: "Login" });
-  } else {
-    next();
-  }
-});
+// router.beforeEach((to, from, next) => {
+//   if (to.name !== "Login") {
+//     next({ name: "Login" });
+//   } else {
+//     next();
+//   }
+// });
